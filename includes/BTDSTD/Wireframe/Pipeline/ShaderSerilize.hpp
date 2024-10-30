73e06a8872578aebc8d0b77c7029cddd110d378d
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2024-10-30 01:26:48 -0400

    added JSON writing and loading

--- includes/BTDSTD/Wireframe/Pipeline/ShaderSerilize.hpp
+++ includes/BTDSTD/Wireframe/Pipeline/ShaderSerilize.hpp
@@ -20,12 +20,70 @@ namespace Wireframe::Shader::Serilize
 
 		Util::ShaderStage stage = Util::ShaderStage::Vertex; //the shader stage this shader belongs to
 
+		//converts this struct into a JSON object
+		inline nlohmann::json ToJSON(const bool isProductionBuild) const
+		{
+			nlohmann::json j;
+
+			j["isProductionBuild"] = isProductionBuild; //are we a production build file
+
+			//if we're in not in production
+			if (!isProductionBuild)
+			{
+				j["name"] = name;
+				j["sourceFP"] = sourceFilepath;
+			}
+
+			j["binaryFP"] = binaryFilepath;
+			j["entryFunc"] = entryPointFuncName;
+			j["stage"] = (uint8_t)stage;
+
+			return j;
+		}
+
+		//converts this struct into a JSON object and appends it
+		inline void AppendToJSON(nlohmann::json& jsonToAppendTo, const bool isProductionBuild) const
+		{
+			nlohmann::json j;
+			j["isProductionBuild"] = isProductionBuild; //are we a production build file
+
+			//if we're in not in production
+			if (!isProductionBuild)
+			{
+				j["name"] = name;
+				j["sourceFP"] = sourceFilepath;
+			}
+
+			j["binaryFP"] = binaryFilepath;
+			j["entryFunc"] = entryPointFuncName;
+			j["stage"] = (uint8_t)stage;
+
+			jsonToAppendTo["shaders"].emplace_back(j);
+		}
+
+		//parses a JSON object into this struct
+		inline void FromJSON(const nlohmann::json& j)
+		{
+			const bool isProductionBuild = j["isProductionBuild"];
+
+			//if we're in not in production
+			if (!isProductionBuild)
+			{
+				name = j["name"];
+				sourceFilepath = j["sourceFP"];
+			}
+
+			binaryFilepath = j["binaryFP"];
+			entryPointFuncName = j["entryFunc"];
+			stage = j["stage"];
+		}
+
 		//gets the defined extension for this custom filetype
 		static inline std::string GetExtentionStr() { return "btdshadersettings"; }
 	};
 
 	//writes shader settings to a file || if isProductionBuild is true, will not write debug data
-	inline bool WriteShaderDataToFile(BTD::IO::FileInfo& settingsFile, const ShaderSerilizeData& data, const bool isProductionBuild)
+	inline bool WriteShaderDataToFile(BTD::IO::FileInfo& settingsFile, const std::vector<ShaderSerilizeData>& data, const bool isProductionBuild)
 	{
 		//checks if the file has the right extension, if not throw a warning and add it ourself
 		if (settingsFile.extension != ShaderSerilizeData::GetExtentionStr())
@@ -40,28 +98,21 @@ namespace Wireframe::Shader::Serilize
 				settingsFile = BTD::IO::FileInfo(settingsFile.absolutePath + ShaderSerilizeData::GetExtentionStr());
 		}
 
-		//build JSON object
+		//generates JSON blobs and merges them
+		const size_t count = data.size();
 		nlohmann::json j;
+		j["count"] = count;
+		for (size_t i = 0; i < count; ++i)
+			data[i].AppendToJSON(j, isProductionBuild);
 
-		//if we're in not in production
-		if (!isProductionBuild)
-		{
-			j["name"] = data.name;
-			j["sourceFP"] = data.sourceFilepath;
-		}
-
-		j["binaryFP"] = data.binaryFilepath;
-		j["entryFunc"] = data.entryPointFuncName;
-		j["stage"] = (uint8_t)data.stage;
-
-		//write to file
+		//build json and write to file
 		BTD::IO::File::WriteWholeTextFile(settingsFile, j.dump());
 
 		return true;
 	}
 
 	//loads shader settings from a file || if isProductionBuild is true, will not load debug data
-	inline bool LoadShaderDataFromFile(const BTD::IO::FileInfo& settingsFile, ShaderSerilizeData& data, const bool isProductionBuild)
+	inline bool LoadShaderDataFromFile(const BTD::IO::FileInfo& settingsFile, std::vector<ShaderSerilizeData>& data, const bool isProductionBuild)
 	{
 		//checks if the file has the right extension, if not error out
 		if (settingsFile.extension != ShaderSerilizeData::GetExtentionStr())
@@ -71,10 +122,11 @@ namespace Wireframe::Shader::Serilize
 			return false;
 		}
 
-		//parse JSON object
+		//parse JSON object and load data
 		nlohmann::json j = nlohmann::json::parse(BTD::IO::File::ReadWholeTextFile(settingsFile).data);
+		const size_t count = j["count"];
 		fmt::print("{}\n", j.dump());
-
+		
 		return true;
 	}
 }

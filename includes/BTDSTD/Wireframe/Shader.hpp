4169eab7156a888686e425d324c2857042122bb1
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2024-11-01 17:31:20 -0400

    merged serilize files into the objects they serilize for less headers

--- includes/BTDSTD/Wireframe/Shader.hpp
+++ includes/BTDSTD/Wireframe/Shader.hpp
@@ -3,7 +3,7 @@
 //defines shaders
 
 #include <BTDSTD/Logger.hpp>
-
+#include <BTDSTD/IO/File.hpp>
 #include <BTDSTD/Wireframe/Core/GPU.hpp>
 #include <BTDSTD/Wireframe/Util/ShaderStages.hpp>
 
@@ -88,38 +88,101 @@ namespace Wireframe::Shader
 
 		return info;
 	}
+}
 
-	//generates a vertex shader stage
-	//static inline VkPipelineShaderStageCreateInfo GenerateVertexShaderStage(const ShaderModule& shader, const char* entryPointFuncName = "main")
-	//{
-	//	VkPipelineShaderStageCreateInfo info{};
-	//	info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-	//	info.pNext = nullptr;
+#include <BTDSTD/Formats/json.hpp>
 
-	//	//shader stage
-	//	info.stage = VK_SHADER_STAGE_VERTEX_BIT;
-	//	//module containing the code for this shader stage
-	//	info.module = shader.shaderModule;
-	//	//the entry point of the shader
-	//	info.pName = entryPointFuncName;
+namespace Wireframe::Shader::Serilize
+{
+	//defines a shader serilize object
+	struct ShaderSerilizeData
+	{
+		std::string name = "", //used for debug tracking || NOT FOR PRODUCTION BUILDS
+			sourceFilepath = "", //where the source code file is located || NOT FOR PRODUCTION BUILDS
+			binaryFilepath = "", //binary SPRV file for shader bytecode, normally relative to the settings file loaded in
+			entryPointFuncName = "main"; //the name of the function to call as a entry point in the shader, normally main
 
-	//	return info;
-	//}
+		Util::ShaderStage stage = Util::ShaderStage::Vertex; //the shader stage this shader belongs to
 
-	////generates a fragment shader stage
-	//static inline VkPipelineShaderStageCreateInfo GenerateFragmentShaderStage(const ShaderModule& shader, const char* entryPointFuncName = "main")
-	//{
-	//	VkPipelineShaderStageCreateInfo info{};
-	//	info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-	//	info.pNext = nullptr;
+		//converts this struct into a JSON object
+		inline nlohmann::json ToJSON(const bool isProductionBuild) const
+		{
+			nlohmann::json j;
 
-	//	//shader stage
-	//	info.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
-	//	//module containing the code for this shader stage
-	//	info.module = shader.shaderModule;
-	//	//the entry point of the shader
-	//	info.pName = entryPointFuncName;
+			j["isProductionBuild"] = isProductionBuild; //are we a production build file
 
-	//	return info;
-	//}
+			//if we're in not in production
+			if (!isProductionBuild)
+			{
+				j["name"] = name;
+				j["sourceFP"] = sourceFilepath;
+			}
+
+			j["binaryFP"] = binaryFilepath;
+			j["entryFunc"] = entryPointFuncName;
+			j["stage"] = (uint8_t)stage;
+
+			return j;
+		}
+
+		//parses a JSON object into this struct
+		inline void FromJSON(const nlohmann::json& j)
+		{
+			const bool isProductionBuild = j["isProductionBuild"];
+
+			//if we're in not in production
+			if (!isProductionBuild)
+			{
+				name = j["name"];
+				sourceFilepath = j["sourceFP"];
+			}
+
+			binaryFilepath = j["binaryFP"];
+			entryPointFuncName = j["entryFunc"];
+			stage = j["stage"];
+		}
+
+		//gets the defined extension for this custom filetype
+		static inline std::string GetExtentionStr() { return "btdshadersettings"; }
+	};
+
+	//writes shader settings to a file || if isProductionBuild is true, will not write debug data
+	inline bool WriteShaderDataToFile(const BTD::IO::FileInfo& settingsFile, const ShaderSerilizeData& data, const bool isProductionBuild)
+	{
+		//checks if the file has the right extension, if not throw a warning and add it ourself
+		BTD::IO::FileInfo f = settingsFile;
+		if (settingsFile.extension != ShaderSerilizeData::GetExtentionStr())
+		{
+			fmt::print("Wireframe Shader Warning: Serilize || WriteShaderDataToFile || \"{}\" does not end in .{}, this is the file extension for Wireframe Shader Setting files. This warning can be ignored as we will add the extension. But to make it go away, add it to your file. The ShaderSerilizeData struct contains a static funtion for getting the correct file extension.\n",
+				settingsFile.absolutePath, ShaderSerilizeData::GetExtentionStr());
+
+			//if we need a period
+			if (settingsFile.absolutePath[settingsFile.absolutePath.size() - 1] != '.')
+				f = BTD::IO::FileInfo(settingsFile.absolutePath + "." + ShaderSerilizeData::GetExtentionStr());
+			else
+				f = BTD::IO::FileInfo(settingsFile.absolutePath + ShaderSerilizeData::GetExtentionStr());
+		}
+
+		//build json and write to file
+		BTD::IO::File::WriteWholeTextFile(f, data.ToJSON(isProductionBuild).dump());
+
+		return true;
+	}
+
+	//loads shader settings from a file || if isProductionBuild is true, will not load debug data
+	inline bool LoadShaderDataFromFile(const BTD::IO::FileInfo& settingsFile, ShaderSerilizeData& data, const bool isProductionBuild)
+	{
+		//checks if the file has the right extension, if not error out
+		if (settingsFile.extension != ShaderSerilizeData::GetExtentionStr())
+		{
+			fmt::print("Wireframe Shader Error: Serilize || LoadShaderDataToFile || \"{}\" does not end in .{}, this is the file extension for Wireframe Shader Setting files. We can not proove this is a valid Shader Settings file. Call \"WriteShaderDataToFile\" to generate a proper file. If the file you are loading was made in a earlier version of the STD, please try updating the file with the newest functions.\n",
+				settingsFile.absolutePath, ShaderSerilizeData::GetExtentionStr());
+			return false;
+		}
+
+		//parse JSON object and load data
+		data.FromJSON(nlohmann::json::parse(BTD::IO::File::ReadWholeTextFile(settingsFile).data));
+
+		return true;
+	}
 }
